732ceb490a25283ed58d4d4124c566f3c81d2a91
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2025-10-05 20:57:02 +0200

    service added

--- order_service/src/app.ts
+++ order_service/src/app.ts
@@ -4,8 +4,10 @@ dotenv.config();
 import express from "express";
 import { createServer } from "node:http";
 import cors from "cors";
-//rabbit helper 
+//rabbit helper
 import { connectRabbit } from "./rabbit.js";
+//for now it is a memory store
+import { orders } from "./store.js";
 //routes
 import indexRouter from "./routes/index.js";
 
@@ -20,13 +22,49 @@ app.use(cors());
 
 //route apply
 app.use("/", indexRouter);
-//start rabit publisher 
+//start rabit publisher
 async function start() {
-    await connectRabbit();
-    //listen here since it is asnyc 
-    app.listen(PORT,()=> {
-        console.log(`order service at : ${PORT}`)
-    })
+  const channel = await connectRabbit();
+  //create exchange and queue
+  const exchange = "order_exchange";
+  await channel.assertExchange(exchange, "fanout", { durable: true });
+  //queue
+  const queue = "order_status_queue";
+  await channel.assertQueue(queue, { durable: true });
+  await channel.bindQueue(queue, exchange, "");
+
+  //listen for confirm or rejection
+  channel.consume(queue, (msg) => {
+    if (!msg) return;
+    const event = JSON.parse(msg.content.toString());
+    //debug
+    console.log("order service received", event);
+
+    //check if it is confirmed or reject and send message
+
+    //first confirmed
+    if (event.type === "order.confirmed") {
+      const { orderId } = event.data;
+      orders.set(orderId, "CONFIRMED");
+      console.log(` Order ${orderId} confirmed`);
+    }
+
+    //reject
+    if (event.type === "order.rejected") {
+      const { orderId } = event.data;
+      //set
+      orders.set(orderId, "REJECTED");
+      console.log(`Order ${orderId} rejected`);
+    }
+
+    //remove from queue
+    channel.ack(msg);
+  });
+
+  //listen here since it is asnyc
+  app.listen(PORT, () => {
+    console.log(`order service at : ${PORT}`);
+  });
 }
 
 await start();
